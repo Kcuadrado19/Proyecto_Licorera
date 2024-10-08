e8b982c83926fffb4e7cbf6a8fa5f067647fe9b1
Jesús David Suárez <jesusd0899@gmail.com>
2024-10-07 19:54:50 -0500

    Creación de Tablas

---
+++ Proyecto_Licorera_Corchos.web/Data/Entities/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Licorera_Corchos.web.Data.Entities
+{
+    public class Clientes
+    {
+        [Key] // Indica que es clave primaria
+        public int Id_Cliente { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo '{0}' debe tener máximo '{1}' carácteres")] // Valida que el campo no supere 50 caracteres
+        [Required(ErrorMessage = "El campo '{0}' es requerido.")] // Indica que es un campo requerido
+        public string Nombre { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo '{0}' debe tener máximo '{1}' carácteres")] // Valida que el campo no supere 50 caracteres
+        [Required(ErrorMessage = "El campo '{0}' es requerido.")] // Indica que es un campo requerido
+        public string Telefono { get; set; }
+    }
+}
