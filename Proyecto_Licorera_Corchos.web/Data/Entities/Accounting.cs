4c93283db89e46029f18d1706c0ed73da46c1b17
JDavid0899 <100887715+JDavid0899@users.noreply.github.com>
2024-10-10 12:20:06 -0500

    Revert "Merge pull request #38 from Kcuadrado19/master"
    This reverts commit 1f7a7f8785cb294b557981cfab765d1a3b4a8a3b, reversing
    changes made to f16bff6503b0b0df845fcc797303cfd81a951207.

--- Proyecto_Licorera_Corchos.web/Data/Entities/Accounting.cs
+++ Proyecto_Licorera_Corchos.web/Data/Entities/Accounting.cs
@@ -5,11 +5,11 @@ namespace Proyecto_Licorera_Corchos.web.Data.Entities
     public class Accounting
     {
         [Key] // Clave primaria
-        public int Id_Accounting { get; set; }
+        public int Id_A { get; set; }
 
         [MaxLength(50, ErrorMessage = "El campo '{0}' debe tener máximo '{1}' caracteres")]
         [Required(ErrorMessage = "El campo '{0}' es requerido.")]
-        public required string NombreAc { get; set; }
+        public string Nombre_A { get; set; }
 
         [MaxLength(100, ErrorMessage = "El campo '{0}' debe tener máximo '{1}' caracteres")]
         [Required(ErrorMessage = "El campo '{0}' es requerido.")]
@@ -17,8 +17,9 @@ namespace Proyecto_Licorera_Corchos.web.Data.Entities
 
         // Relación con la tabla Modificaciones
         public int Id_HModificaciones_A { get; set; }
+        public virtual Modifications Modificaciones { get; set; }
 
-        public virtual required Modifications Modificaciones { get; set; }
+
     }
 
 }
