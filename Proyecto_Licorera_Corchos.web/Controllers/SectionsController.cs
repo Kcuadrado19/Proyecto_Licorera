435f9fbcf96613ebf89fc373781b7363b4692493
Kcuadrado19 <167935450+Kcuadrado19@users.noreply.github.com>
2024-10-24 00:04:46 -0500

    se creo la clase Section y su respectivo controller, ademas de su index.

---
+++ Proyecto_Licorera_Corchos.web/Controllers/SectionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto_Licorera_Corchos.web.Data.Entities;
+
+
+namespace Proyecto_Licorera_Corchos.web.Controllers
+{
+    public class SectionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Section> sections = new List<Section>
+
+            {
+                new Section
+                {
+                    Id = 1,
+                    Name = "Base",
+                    Description = "Test 1"
+                },
+
+                new Section
+                {
+                    Id = 1,
+                    Name = "Hacking",
+                    Description = "Test 2"
+                },
+
+                new Section
+                {
+                    Id = 1,
+                    Name = "Telecomunicaciones",
+                    Description = "Test 3"
+                },
+
+
+            };
+
+            return View(sections);
+        }
+    }
+}
