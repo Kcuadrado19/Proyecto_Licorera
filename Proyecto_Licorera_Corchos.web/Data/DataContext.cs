810e3edc197a1bb5d6b336e01820d2b75b5a2a23
leidymahecha16 <leidymahecha761@gmail.com>
2024-11-09 19:39:08 -0500

    crud

--- Proyecto_Licorera_Corchos.web/Data/DataContext.cs
+++ Proyecto_Licorera_Corchos.web/Data/DataContext.cs
@@ -4,19 +4,20 @@ using Proyecto_Licorera_Corchos.web.Data.Entities;
 namespace Proyecto_Licorera_Corchos.web.Data
 {
     public class DataContext : DbContext
-
     {
-        public DataContext(DbContextOptions<DataContext>options) : base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            
         }
 
+        // DbSet para Products
         public DbSet<Products> Products { get; set; }
 
+        // DbSet para Sales
         public DbSet<Sales> Sales { get; set; }
 
-        public DbSet<Users> Users { get; set; }
-      
-    
+        // DbSet para User
+        public DbSet<User> Users { get; set; } // Cambié 'Users' a 'User'
     }
 }
+
+
---
+++ Proyecto_Licorera_Corchos.web/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto_Licorera_Corchos.web.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; } // Clave primaria
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
+
