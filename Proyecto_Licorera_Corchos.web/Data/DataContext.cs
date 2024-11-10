7a2ea31f65e1ec6878f4094c16043e7e3a546592
Kcuadrado19 <167935450+Kcuadrado19@users.noreply.github.com>
2024-11-10 13:20:08 -0500

    arreglado nombre de producto

--- Proyecto_Licorera_Corchos.web/Data/DataContext.cs
+++ Proyecto_Licorera_Corchos.web/Data/DataContext.cs
@@ -11,7 +11,7 @@ namespace Proyecto_Licorera_Corchos.web.Data
         }
 
         // DbSet para tus otras entidades
-        public DbSet<Products> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
         public DbSet<Sales> Sales { get; set; }
     }
 }
